1aac2572fa572652402f691c16a7dd1d1be351e2
Arun Patidar <arunpatidar26@gmail.com>
2018-09-01 22:10:42 +0200

    3
    3

--- java/ContentFragmentContent.java
+++ java/ContentFragmentContent.java
@@ -3,44 +3,35 @@ package com.aem.community.core.components;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
 import javax.jcr.Node;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.adobe.cq.sightly.WCMUsePojo;
 
 public class ContentFragmentContent extends WCMUsePojo {
-
 	private static final Logger log = LoggerFactory.getLogger(ContentFragmentContent.class);
-
 	private String content = "";
 
 	@Override
 	public void activate() {
 		String cfInput = get("cfInput", String.class);
-		String variation="";
-		try  {
-			variation = get("variation", String.class);
-			if(variation.equals("master")) {
-				variation = "original";	
-			}
-		}
-		catch(Exception e) {
-			variation = "original";	
-		}
-		
-		cfInput = cfInput + "/jcr:content/renditions/"+variation+"/jcr:content";
-		
-		log.info(cfInput);
-		
+		String variation = "";
 		try {
-			Node cdNode = getResource().getResourceResolver().getResource(cfInput).adaptTo(Node.class);
-			if (cdNode != null) {
-				if (cdNode.hasProperty("jcr:data") && cdNode.hasProperty("jcr:mimeType")) {
-					if (cdNode.getProperty("jcr:mimeType").getString().equals("text/html")) {
-						InputStream in = cdNode.getProperty("jcr:data").getBinary().getStream();
+			variation = get("variation", String.class);
+			if (variation.equals("master")) {
+				variation = "original";
+			}
+		} catch (Exception e) {
+			variation = "original";
+		}
+		cfInput = cfInput + "/jcr:content/renditions/" + variation + "/jcr:content";
+		try {
+			Node cfNode = getResource().getResourceResolver().getResource(cfInput).adaptTo(Node.class);
+			if (cfNode != null) {
+				if (cfNode.hasProperty("jcr:data") && cfNode.hasProperty("jcr:mimeType")) {
+					if (cfNode.getProperty("jcr:mimeType").getString().equals("text/html")) {
+						InputStream in = cfNode.getProperty("jcr:data").getBinary().getStream();
 						BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 						StringBuilder out = new StringBuilder();
 						String line;
@@ -50,19 +41,18 @@ public class ContentFragmentContent extends WCMUsePojo {
 						reader.close();
 						content = out.toString();
 					} else {
-						log.info("Content Fragment asset file is not selected");
+						log.info("Content Fragment is not selected");
 					}
 				} else {
-					log.info("no data or mime type property set");
+					log.info("Either jcr:data or mime type property set");
 				}
 			} else {
-				log.info("Content Fragment master content doesn't exist");
+				log.info("Content Fragment doesn't exist");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
 	public String getContent() {
 		return content;
 	}
