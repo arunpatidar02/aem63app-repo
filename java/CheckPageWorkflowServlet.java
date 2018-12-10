a9e3466d0f1fe465b176862959fcbf06a005ede8
Arun Patidar <arunpatidar26@gmail.com>
2018-12-10 21:10:41 +0530

    1
    1

---
+++ java/CheckPageWorkflowServlet.java
@@ -0,0 +1,48 @@
+package com.aem.community.core.servlets;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.servlets.HttpConstants;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+
+import com.day.cq.workflow.exec.Workflow;
+import com.day.cq.workflow.status.WorkflowStatus;
+
+@Component(service = Servlet.class, property = { Constants.SERVICE_DESCRIPTION + "= Check Workflow Status Servlet",
+		"sling.servlet.methods=" + HttpConstants.METHOD_GET, "sling.servlet.paths=" + "/bin/page/checkWfStatus"
+ })
+public class CheckPageWorkflowServlet extends SlingSafeMethodsServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(final SlingHttpServletRequest req, final SlingHttpServletResponse resp)
+			throws ServletException, IOException {
+		final String page = "/content/AEM63App/fr/testJS";
+		Resource rs = req.getResourceResolver().getResource(page);
+		WorkflowStatus ws = rs.adaptTo(WorkflowStatus.class);
+		boolean status = ws.isInRunningWorkflow(true);
+		resp.setContentType("text/html");
+		resp.getWriter().write("Workflow Found : "+status);
+		if(status) {
+			resp.getWriter().write("<br>Workflows are : "+status);
+			List<Workflow> wfList =ws.getWorkflows(true);
+			Iterator<Workflow> it = wfList.iterator();
+			while(it.hasNext()) {
+				resp.getWriter().write("Workflow Instance Id : "+it.next().getId());
+			}
+		}
+		resp.getWriter().close();
+
+	}
+}
