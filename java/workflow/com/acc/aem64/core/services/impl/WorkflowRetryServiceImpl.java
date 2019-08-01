96910ebff976f779b82f8bd63af3ca001ad64603
arunpatidar02 <35201853+arunpatidar02@users.noreply.github.com>
2019-08-01 16:46:18 +0200

    2

--- java/workflow/com/acc/aem64/core/services/impl/WorkflowRetryServiceImpl.java
+++ java/workflow/com/acc/aem64/core/services/impl/WorkflowRetryServiceImpl.java
@@ -1,9 +1,13 @@
 package com.acc.aem64.core.services.impl;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,13 +21,16 @@ import com.adobe.granite.workflow.exec.WorkItem;
 public class WorkflowRetryServiceImpl implements WorkflowRetryService {
 
 	protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+	private static final String SUB_SERVICE = "adminService";
+
+	@Reference
+	private ResourceResolverFactory resourceFactory;
 
 	@Override
-	public void retryWorkflow(WorkflowSession wfSession, WorkItem workflowItem,
-			Map<String, String> param) {
+	public void retryWorkflow(WorkflowSession wfSession, WorkItem workflowItem, Map<String, String> param) {
 		try {
 			if (wfSession != null) {
-				advance(wfSession, workflowItem,  param);
+				advance(wfSession, workflowItem, param);
 			} else {
 				logger.warn("[retryWorkflow] : Workflow session is null");
 			}
@@ -31,6 +38,28 @@ public class WorkflowRetryServiceImpl implements WorkflowRetryService {
 			e.printStackTrace();
 			logger.info("Exception " + e.getMessage());
 		}
+	}
+
+	@Override
+	public void retryWorkflow(String workflowItemPath, Map<String, String> param) {
+		ResourceResolver resourceResolver = null;
+		try {
+			resourceResolver = getResourceResolver();
+			WorkflowSession wfSession = (WorkflowSession) resourceResolver.adaptTo(WorkflowSession.class);
+			WorkItem workflowItem;
+			if (wfSession != null) {
+				workflowItem = wfSession.getWorkItem(workflowItemPath);
+				advance(wfSession, workflowItem, param);
+			} else {
+				logger.warn("[retryWorkflow] : Workflow session is null");
+			}
+		} catch (WorkflowException | IOException e) {
+			e.printStackTrace();
+			logger.info("Exception " + e.getMessage());
+		} finally {
+			closeResourceResolver(resourceResolver);
+		}
+
 	}
 
 	private void advance(WorkflowSession wfSession, WorkItem workflowItem, Map<String, String> param)
@@ -47,6 +76,27 @@ public class WorkflowRetryServiceImpl implements WorkflowRetryService {
 		if (item != null) {
 			Route loopRoute = new LoopbackRoute(item);
 			wfSession.complete(item, loopRoute);
+		}
+	}
+
+	// get ResourceResolver
+	public ResourceResolver getResourceResolver() {
+		ResourceResolver resourceResolver = null;
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put(ResourceResolverFactory.SUBSERVICE, SUB_SERVICE);
+		try {
+			resourceResolver = resourceFactory.getServiceResourceResolver(paramMap);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.debug("getResourceResolver : Unable to ResourceResolver : " + e);
+		}
+		return resourceResolver;
+	}
+
+	// Close ResourceResolver
+	public void closeResourceResolver(ResourceResolver rr) {
+		if (rr != null) {
+			rr.close();
 		}
 	}
 
