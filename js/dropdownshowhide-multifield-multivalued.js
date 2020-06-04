ab42591bf257082e88495fb90a7879c997b29bdf
Arun Patidar <35201853+arunpatidar02@users.noreply.github.com>
2020-06-04 22:43:31 +0200

    Create dropdownshowhide-multifield-multivalued.js

---
+++ js/dropdownshowhide-multifield-multivalued.js
@@ -0,0 +1,74 @@
+/**
+ * Extension to the standard dropdown/select component. It enabled hidding/unhidding of other components based on the
+ * selection made in the dropdown/select in multifield and maultivalued.
+ *
+ * How to use:
+ *
+ * - add the class cq-dialog-dropdown-showhide-multival to the dropdown/select element
+ * - add the data attribute cq-dialog-dropdown-showhide-target to the dropdown/select element, value should be the
+ *   selector, usually a specific class name, to find all possible target elements that can be shown/hidden.
+ * - add the target class to each target component that can be shown/hidden
+ * - add the class hidden to each target component to make them initially hidden
+ * - add the attribute showhidetargetvalue to each target component, the value(or commma separated multiple values e.g. val1, val2) should equal the value of the select
+ *   option that will unhide this element.
+ */
+
+(function(document, $) {
+    "use strict";
+
+    // when dialog gets injected
+    $(document).on("foundation-contentloaded", function(e) {
+        // if there is already an inital value make sure the according target element becomes visible
+        showHideHandler($(".cq-dialog-dropdown-showhide-multival", e.target));
+    });
+
+    $(document).on("selected", ".cq-dialog-dropdown-showhide-multival", function(e) {
+        showHideHandler($(this));
+    });
+
+    function showHideHandler(el) {
+        el.each(function(i, element) {
+            if ($(element).is("coral-select")) {
+                // handle Coral3 base drop-down
+                Coral.commons.ready(element, function(component) {
+                    showHide(component, element);
+                    component.on("change", function() {
+                        showHide(component, element);
+                    });
+                });
+            } else {
+                // handle Coral2 based drop-down
+                var component = $(element).data("select");
+                if (component) {
+                    showHide(component, element);
+                }
+            }
+        })
+    }
+
+    function showHide(component, element) {
+        // get the selector to find the target elements. its stored as data-.. attribute
+        var target = $(element).data("cqDialogDropdownShowhideTarget");
+        var $target = $(target);
+        var elementIndex = $(element).closest('coral-multifield-item').index();
+
+        if (target) {
+            var value;
+            if (component.value) {
+                value = component.value;
+            } else {
+                value = component.getValue();
+            }
+            $target.each(function(index) {
+                var tarIndex = $(this).closest('coral-multifield-item').index();
+                if (elementIndex == tarIndex) {
+                    $(this).not(".hide").addClass("hide");
+                    $(this).filter(function() {
+  						return $(this).data('showhidetargetvalue').replace(/ /g, '').split(',').includes(value);
+					}).removeClass("hide");
+                }
+            });
+        }
+    }
+
+})(document, Granite.$);
