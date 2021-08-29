fc406d978441596bfd655c9689584dd710727b85
Arun Patidar <35201853+arunpatidar02@users.noreply.github.com>
2021-08-29 15:56:42 +0200

    Update UploadAssets.java

--- java/UploadAssets.java
+++ java/UploadAssets.java
@@ -4,54 +4,71 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.rmi.ServerException;
+import java.io.PrintWriter;
 
-import javax.jcr.Session;
 import javax.servlet.Servlet;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.ModifiableValueMap;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.api.servlets.HttpConstants;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Component;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import com.day.cq.commons.jcr.JcrConstants;
+import com.day.cq.dam.api.Asset;
 import com.day.cq.dam.api.AssetManager;
 
 @Component(service = Servlet.class, property = { Constants.SERVICE_DESCRIPTION + "= Upload Asset Servlet",
-		"sling.servlet.methods=" + HttpConstants.METHOD_GET, "sling.servlet.paths=" + "/bin/upload/assets" })
+    "sling.servlet.methods=" + HttpConstants.METHOD_GET, "sling.servlet.paths=" + "/bin/upload/asset"})
 public class UploadAssets extends org.apache.sling.api.servlets.SlingAllMethodsServlet {
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	protected final Logger log = LoggerFactory.getLogger(this.getClass());
+  @Override
+  protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
+    response.setHeader("Content-Type", "text/html");
+    PrintWriter pw = response.getWriter();
+    pw.write("<h1>Reading image from desktop and injecting image to JCR DAM</h1>");
+    try {
+      ResourceResolver r = request.getResourceResolver();
+      AssetManager assetMgr = r.adaptTo(AssetManager.class);
+      InputStream is = null;
+      String fileName = "assest-metadata.PNG";
+      String mimeType = "image/png";
+      File fi = new File("/Users/arunpatidar/Downloads/" + fileName);
 
-	@Override
-	protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
-			throws ServerException, IOException {
-		response.setHeader("Content-Type", "text/html");
-		response.getWriter().print("<h1>Reading image from desktop and injecting image to JCR DAM</h1>");
-		try {
-			AssetManager assetMgr = request.getResourceResolver().adaptTo(AssetManager.class);
-			InputStream is = null;
-			String fileName ="giphy.gif";
-			String mimeType="image/gif";
-			File fi = new File("/Users/arunpatidar/Downloads/"+fileName);
+      is = new FileInputStream(fi);
+      String newFile = "/content/dam/we-retail/" + fileName;
+      Asset asset = assetMgr.createAsset(newFile, is, mimeType, true);
+      saveCustomMetadataInfo(pw, asset);
+      r.commit();
+      pw.write("<p>File uploaded</p>");
+    }
+    catch (Exception e) {
+      pw.write(e.getMessage());
+    }
+  }
 
-			is = new FileInputStream(fi);
-			String newFile = "/content/dam/AEM63App/"+fileName;
-			Session session = request.getResourceResolver().adaptTo(Session.class);
-			assetMgr.createAsset(newFile, is, mimeType, true);
-			session.save();
-			session.logout();
-			response.getWriter().print("<p>File uploaded</p>");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		finally {
-			response.getWriter().close();
-		}
-	}
+  void saveCustomMetadataInfo(PrintWriter pw, final Asset asset) {
+    Resource assetResource = asset.adaptTo(Resource.class);
+    String customPropName = "prop1";
+    String customPropValue = "prop1Val";
+
+    if (assetResource != null) {
+      assetResource = assetResource.getChild(JcrConstants.JCR_CONTENT + "/metadata");
+    }
+
+    if (assetResource != null) {
+      ValueMap assetProperties = assetResource.adaptTo(ModifiableValueMap.class);
+
+      if (assetProperties != null) {
+        assetProperties.put(customPropName, customPropValue);
+        pw.write("<p>Metadta is set</p>");
+      }
+    }
+  }
 
 }
